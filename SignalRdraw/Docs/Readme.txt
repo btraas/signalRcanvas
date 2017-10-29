30dba9bbb5b9b3459df4255bc35af7d701fcf9b4
Brayden Traas <btraas@gmail.com>
2017-10-29 13:52:48 -0700

    init

---
+++ SignalRdraw/Docs/Readme.txt
@@ -0,0 +1,3 @@
+﻿http://www.c-sharpcorner.com/UploadFile/jj12345678910/simple-drawing-signalr-using-visual-studio-2012/
+
+Install-Package Microsoft.aspNet.SignalR
---
+++ SignalRdraw/DrawDot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRdraw
+{
+    public class DrawDot : Hub
+    {
+        public void UpdateCanvas(int x, int y)
+        {
+            Clients.All.updateDot(x, y);
+        }
+
+        public void ClearCanvas()
+        {
+            Clients.All.clearCanvas();
+        }
+
+    }
+}
